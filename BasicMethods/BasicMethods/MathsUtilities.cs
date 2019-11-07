4c92d7c87938792aabc28e09f405e6af2c872b57
Dominic -Eagle Eye <W61504@student.wsiz.rzeszow.pl>
2019-11-07 18:46:44 +0100

    MathUtils

---
+++ BasicMethods/BasicMethods/MathsUtilities.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BasicMethods
+{
+   public static class MathsUtilities
+    {
+        public static double SolveLinerEquation(double a,double b)
+        {
+            return a == 0 ? 0 : -b / a;//important stuff'?'
+            if(a==0)
+         
+                return 0;
+
+                return -b / a;
+            
+        }
+    }
+}
---
+++ BasicMethods/BasicMethods/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BasicMethods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {            Console.WriteLine(MathsUtilities.SolveLinerEquation(3, 6));
+           
+        }
+    }
+}
