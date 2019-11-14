02318808e3b60367dd27c64a4e5b1f18fbde2d7e
Dominic_Spyeye <W61504@student.rzeszow.wsiz.pl>
2019-11-14 17:55:22 +0100

    basic method

--- BasicMethods/BasicMethods/MathsUtilities.cs
+++ BasicMethods/BasicMethods/MathsUtilities.cs
@@ -4,17 +4,36 @@ using System.Text;
 
 namespace BasicMethods
 {
-   public static class MathsUtilities
+    public static class MathsUtilities
     {
-        public static double SolveLinerEquation(double a,double b)
+        public static double SolveLinerEquation(double a, double b)
         {
             return a == 0 ? 0 : -b / a;//important stuff'?'
-            if(a==0)
-         
-                return 0;
+            //if (a == 0)
 
-                return -b / a;
-            
+            //    return 0;
+
+            //return -b / a;
+        }
+        public static int GetBiggerNumber(int a, int b)
+        {
+            return a > b ? a : b;
+
+            //return a;
+            //return b;
+        }
+            public static int GCD(int a, int b)
+            {
+                while (a != b)
+                {
+                    if (a > b)
+                        a = a - b;
+                    else b -= a;
+                }
+                return a;
+
+
+            }
         }
     }
-}
+
--- BasicMethods/BasicMethods/Program.cs
+++ BasicMethods/BasicMethods/Program.cs
@@ -1,12 +1,15 @@
 ﻿using System;
-
+using static BasicMethods.MathsUtilities;
 namespace BasicMethods
 {
     class Program
     {
         static void Main(string[] args)
-        {            Console.WriteLine(MathsUtilities.SolveLinerEquation(3, 6));
-           
+        {
+            Console.WriteLine(SolveLinerEquation(3, 6));
+            Console.WriteLine(GetBiggerNumber(10, 6));
+            Console.WriteLine(GCD(56,49));
+
         }
     }
 }
