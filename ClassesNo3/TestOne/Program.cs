2c2002436aaae7144789eef9799f1ffe553e71d2
W61504 <W61504@student.wsiz.rzeszow.pl>
2019-11-15 18:18:16 +0100

    testone

---
+++ ClassesNo3/TestOne/Program.cs
@@ -0,0 +1,28 @@
+﻿
+using System;
+
+ 
+namespace testone
+    {
+        class Program
+        {
+            static void Main(string[] args)
+            {
+                int i;
+                Console.Write("Enter a Number : ");
+                i = int.Parse(Console.ReadLine());
+                if (i % 2 == 0)
+                {
+                    Console.Write("Entered Number is an Even Number");
+                }
+                else
+                {
+                    Console.Write("Entered Number is an Odd Number");
+                }
+            Console.Read();
+
+        }
+
+
+    }
+}
