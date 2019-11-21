e41b64254dae5434e29c23a27f5b7bfbfa980cd3
Arrays <W61504@student.wsiz.rzeszow.pl>
2019-11-21 17:48:44 +0100

    arrayclasses

--- ArraysStrings/ArraysClasses/Program.cs
+++ ArraysStrings/ArraysClasses/Program.cs
@@ -6,10 +6,28 @@ namespace ArraysClasses
     {
         static void Main(string[] args)
         {
+            //FirstExample();
+            double[] WeekTempertures = {1,2.2,3,4.8,5.9,6,7};
+
+        }
+
+        private static void FirstExample()
+        {
             int[] MyArray = new int[3];
-            foreach(var MyElement in MyArray)
-            Console.WriteLine(MyElement);
-            
+            foreach (var MyElement in MyArray)
+                Console.WriteLine(MyElement);
+
+
+            MyArray[0] = 10;
+            MyArray[1] = 60;
+            MyArray[2] = 40;
+            foreach (var MyElement in MyArray)
+                Console.WriteLine(MyElement);
+            for (int i = 0; i < MyArray.Length; i++)
+                MyArray[i] += 3;
+
+            foreach (var MyElement in MyArray)
+                Console.WriteLine(MyElement);
         }
     }
 }
