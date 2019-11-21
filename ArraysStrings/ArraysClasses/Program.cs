f2593e93b7c24f474f7ffd85d4f257492e110172
Arrays <W61504@student.wsiz.rzeszow.pl>
2019-11-21 18:29:15 +0100

    Arrayclasses

--- ArraysStrings/ArraysClasses/Program.cs
+++ ArraysStrings/ArraysClasses/Program.cs
@@ -8,8 +8,20 @@ namespace ArraysClasses
         {
             //FirstExample();
             //SecondExamples();
-            double[] WeekTemperatures = { 1, 2.2, 3, 4.8, 5.9, 6, 7 };
+            //ThirdExamples();
 
+        }
+
+        private static void ThirdExamples()
+        {
+            double[] WeekTemperatures = { 1, 2.2, 3, 4.8, 5.9, 6, 7 };
+            double[] reverted = new double[7];
+            for (int i = 0; i < WeekTemperatures.Length; i++)
+            {
+                reverted[i] = WeekTemperatures[6 - i];//reverted of array
+
+            }
+            DisplayArray(reverted);
         }
 
         private static void SecondExamples()
@@ -26,19 +38,28 @@ namespace ArraysClasses
         {
             int[] MyArray = new int[3];
             foreach (var MyElement in MyArray)
-                Console.WriteLine(MyElement);
+                DisplayArray(MyArray);
 
 
             MyArray[0] = 10;
             MyArray[1] = 60;
             MyArray[2] = 40;
+
             foreach (var MyElement in MyArray)
-                Console.WriteLine(MyElement);
+                DisplayArray(MyArray);
+
             for (int i = 0; i < MyArray.Length; i++)
                 MyArray[i] += 3;
 
             foreach (var MyElement in MyArray)
-                Console.WriteLine(MyElement);
+                DisplayArray(MyArray);
+
+        }
+        private static void DisplayArray<T>(T[] array)
+        {
+            foreach (var item in array)
+                Console.WriteLine(item);
         }
     }
+
 }
