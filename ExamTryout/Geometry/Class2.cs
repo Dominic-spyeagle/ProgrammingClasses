067a0e77fadd2d6d5fe566ed2f2df0a13b9b620c
W61504 <W61504@RA145B-17>
2020-01-30 18:16:27 +0100

    Task No 2

---
+++ ExamTryout/Geometry/Class2.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Geometry
+{
+    class Geometry
+    {
+        private double x;
+        private double y;
+
+        public double X => x;
+
+        public double Y => y;
+        public Geometry()
+        {
+            x = 0;
+            y = 0; 
+        }
+        public Geometry(double z)
+        {
+            x = z;
+            y = z;
+        }
+
+        public Geometry(double x , double y)
+        {
+            x = x;
+            y = y;
+        }
+    }
+
+}
