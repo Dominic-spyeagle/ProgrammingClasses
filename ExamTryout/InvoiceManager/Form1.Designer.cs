e37f30bf4d61c467aa3c62b8910b08c7b91f703b
W61504 <W61504@RA145B-17>
2020-01-30 18:05:56 +0100

    invoice

---
+++ ExamTryout/InvoiceManager/Form1.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace InvoiceManager
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pathTextBox = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.resultLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // pathTextBox
+            // 
+            this.pathTextBox.Location = new System.Drawing.Point(24, 31);
+            this.pathTextBox.Name = "pathTextBox";
+            this.pathTextBox.Size = new System.Drawing.Size(415, 20);
+            this.pathTextBox.TabIndex = 0;
+            this.pathTextBox.Text = "C:\\Users\\W61504\\Desktop\\Invoice.txt";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(461, 27);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Read";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // resultLabel
+            // 
+            this.resultLabel.AutoSize = true;
+            this.resultLabel.Location = new System.Drawing.Point(24, 58);
+            this.resultLabel.Name = "resultLabel";
+            this.resultLabel.Size = new System.Drawing.Size(0, 13);
+            this.resultLabel.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.resultLabel);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pathTextBox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox pathTextBox;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label resultLabel;
+    }
+}
+
---
+++ ExamTryout/InvoiceManager/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace InvoiceManager
+{
+    public partial class Form1 : Form
+    {
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            string path = pathTextBox.Text;
+            if (!File.Exists(path))
+            {
+                MessageBox.Show("file does exists");
+                return;
+            }
+            string[] content = File.ReadAllLines(path);
+            int totalsum = 0;
+            List<string> dates = new List<string>();
+            for (int i = 1; i < content.Length; i++)
+            {
+                string line = content[i];
+                string[] elements = line.Split('\t');
+                totalsum = totalsum + int.Parse(elements[1]);
+                if (!dates.Contains(elements[0]))
+                {
+                    dates.Add(elements[0]);
+                }
+            }
+            resultLabel.Text = $"Total dates {dates.Count}, total sum {totalsum}";
+        }
+    }
+}
